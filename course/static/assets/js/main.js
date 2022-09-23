9f3334546543300320c13ff074e77901c7cc4228
elvin <elvinyeka@gmail.com>
2022-09-23 14:50:17 +0400

    add event upload img preview style ready

--- course/static/assets/js/main.js
+++ course/static/assets/js/main.js
@@ -2,15 +2,20 @@ const courseLogoUpload = document.querySelector("#id_course_logo");
 const courseLogoImg = document.querySelector("#id_logo_img");
 const courseProfileUpload = document.querySelector("#id_course_profile");
 const courseProfileImg = document.querySelector("#id_profile_img");
+const eventImgUpload = document.querySelector("#id_photo");
+const eventImg = document.querySelector("#id_event_img");
 
 const onChangeImage = (input, img) => {
-  input.addEventListener("change", () => {
-    const [file] = input.files;
-    if (file) {
-      img.src = URL.createObjectURL(file);
-    }
-  });
+  if (input && img) {
+    input.addEventListener("change", () => {
+      const [file] = input.files;
+      if (file) {
+        img.src = URL.createObjectURL(file);
+      }
+    });
+  }
 };
 
 onChangeImage(courseLogoUpload, courseLogoImg);
 onChangeImage(courseProfileUpload, courseProfileImg);
+onChangeImage(eventImgUpload, eventImg);
