8c4a0363a886635c5a57f74e0ca70465b1df83ad
FeqanUmarov <umarovfeqan@gmail.com>
2022-10-01 17:01:07 +0400

    refactored

--- inquiry/views.py
+++ inquiry/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import QueryForm,TutorQueryForm
+from .forms import QueryForm, TutorQueryForm
 from .models import UserQuery, TutorQuery
 from django.contrib import messages
 # Create your views here.
+
+
 def inquirys(request):
     form = QueryForm(request.POST or None)
     if form.is_valid():
@@ -13,16 +15,17 @@ def inquirys(request):
         username = form.cleaned_data.get("username")
         course_name = form.cleaned_data.get("course_name")
         course_email = form.cleaned_data.get("course_email")
-        newquery = UserQuery(name = name, surname=surname,email=email,phone=phone,username = username,course_name=course_name,course_email=course_email)
+        newquery = UserQuery(name=name, surname=surname, email=email, phone=phone,
+                             username=username, course_name=course_name, course_email=course_email)
         newquery.save()
-        messages.success(request,"Sorgu ugurla elave edildi. Qisa zamanda sizinle elaqe saxlanilacaq")
+        messages.success(
+            request, "Sorgu ugurla elave edildi. Qisa zamanda sizinle elaqe saxlanilacaq")
         return redirect("index")
 
-
     contex = {
-        "form":form
+        "form": form
     }
-    return render(request,"inquiry.html",contex)
+    return render(request, "inquiry.html", contex)
 
 
 def teacherquery(request):
@@ -34,13 +37,14 @@ def teacherquery(request):
         phone = form.cleaned_data.get("teacherphone")
         experience = form.cleaned_data.get("experience")
         study_field = form.cleaned_data.get("study_field")
-        newquery = TutorQuery(teachername = name , teachersurname = surname,email=email,teacherphone = phone,experience=experience,study_field=study_field)
+        newquery = TutorQuery(teachername=name, teachersurname=surname, email=email,
+                              teacherphone=phone, experience=experience, study_field=study_field)
         newquery.save()
-        messages.success(request,"Sorgu ugurla elave edildi. Qisa zamanda sizinle elaqe saxlanilacaq")
+        messages.success(
+            request, "Sorgu ugurla elave edildi. Qisa zamanda sizinle elaqe saxlanilacaq")
         return redirect("index")
-        
-        
+
     contex = {
-        "form":form
+        "form": form
     }
-    return render(request,"tutorquery.html",contex)
+    return render(request, "tutorquery.html", contex)
--- mastercourse/settings.py
+++ mastercourse/settings.py
@@ -10,10 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
-from datetime import timedelta
 import os
-from datetime import datetime
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,11 +131,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -152,5 +149,3 @@ CKEDITOR_CONFIGS = {
         "allowedContent": True,
     }
 }
-
-
