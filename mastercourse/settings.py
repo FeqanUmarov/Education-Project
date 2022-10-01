79fb311c08c695255520f6f640655d394013d71a
FeqanUmarov <umarovfeqan@gmail.com>
2022-10-01 19:03:50 +0400

    env vars added

--- mastercourse/settings.py
+++ mastercourse/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'mastercourse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         'NAME': os.environ.get('POSTGRES_NAME'),
