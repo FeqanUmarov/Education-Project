6dc24c30cfc47e9eb8cb8be9274de7ee22f68987
FeqanUmarov <umarovfeqan@gmail.com>
2022-10-14 20:27:25 +0400

    update media

--- mastercourse/settings.py
+++ mastercourse/settings.py
@@ -165,7 +165,7 @@ else:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
 
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
